3e1a8c488266372f77111cd055d26bcec6bfd196
Rodrigo Farah <digofarah@gmail.com>
2024-04-29 11:00:04 -0300

    ForeignKey entre CustomUser e Apartment has been deleted cause there is already a many-to-many relation (and a specific table) between them

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from condo.models import Condominium, Apartment
+from condo.models import Condominium
 
 
 class CustomUser(AbstractUser):
@@ -10,4 +10,3 @@ class CustomUser(AbstractUser):
     )
     user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
     condominium = models.ForeignKey(to=Condominium, on_delete=models.CASCADE)
-    apartment = models.ForeignKey(to=Apartment, on_delete=models.CASCADE)
