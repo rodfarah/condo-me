f479e55cecaef5923cb2a9cd63afca221e472310
Rodrigo Farah <digofarah@gmail.com>
2024-05-02 17:07:11 -0300

    fixing git conflict

--- project/settings.py
+++ project/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 ALLOWED_HOSTS = []
