31ddae9f7d2ef1e52fdfc8b266b15c8788e761b6
Rodrigo Farah <digofarah@gmail.com>
2024-05-03 17:06:09 -0300

    list display for apartment in admin interface

--- condo/admin.py
+++ condo/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Condominium, Block, Apartment, CommonArea
 
+# change Django Administrator (title)
+admin.site.site_header = "Condo-me - Admin Panel"
+
+
 # Register models into admin interface:
 
 
@@ -16,7 +20,8 @@ class BlockAdmin(admin.ModelAdmin):
 
 @admin.register(Apartment)
 class ApartmentAdmin(admin.ModelAdmin):
-    ...
+    # items to be shown in users admin main list
+    list_display = ['number', 'block']
 
 
 @admin.register(CommonArea)
