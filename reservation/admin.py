c707e18cac760cf6da3398fc6d7a6d0bf9586f13
Rodrigo Farah <digofarah@gmail.com>
2024-05-09 09:56:58 -0300

    deleting user app content and everything related with it

--- reservation/admin.py
+++ reservation/admin.py
@@ -4,18 +4,6 @@ from .models import Reservation
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
-    # autocomplete_fields = ["apartments", "users",]
-    # fields = [
-    #     "condominium",
-    #     "common_area",
-    #     "date",
-    #     "start_time",
-    #     "end_time",
-    #     "share_with_others",
-    #     "active",
-    #     "created_at",
-    #     "updated_at"
-    # ]
     readonly_fields = [
         "created_at",
         "updated_at"
