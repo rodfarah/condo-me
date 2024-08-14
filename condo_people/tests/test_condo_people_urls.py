5680be950dddb219088884d1c2c96520d3dec405
Rodrigo Farah <digofarah@gmail.com>
2024-08-14 16:20:31 -0300

    adds a few more condo_people url tests

--- condo_people/tests/test_condo_people_urls.py
+++ condo_people/tests/test_condo_people_urls.py
@@ -22,3 +22,26 @@ class CondoPeopleURLsTest(TestCase):
     def test_condo_people_logout_url_is_correct(self):
         url = reverse("condo_people:logout")
         self.assertEqual(url, "/condo_people/logout/")
+
+    def test_condo_people_password_change_url_is_correct(self):
+        url = reverse("condo_people:password_change")
+        self.assertEqual(url, "/condo_people/password-change/")
+
+    def test_condo_people_password_reset_url_is_correct(self):
+        url = reverse("condo_people:password_reset")
+        self.assertEqual(url, "/condo_people/password-reset/")
+
+    def test_condo_people_password_reset_done_url_is_correct(self):
+        url = reverse("condo_people:password_reset_done")
+        self.assertEqual(url, "/condo_people/password-reset/done/")
+
+    def test_condo_people_password_reset_confirm_url_is_correct(self):
+        url = reverse(
+            "condo_people:password_reset_confirm",
+            kwargs={"uidb64": "dummy_uidb64", "token": "dummy_token"},
+        )
+        self.assertEqual(url, "/condo_people/password-reset/dummy_uidb64/dummy_token/")
+
+    def test_condo_people_password_reset_complete_url_is_correct(self):
+        url = reverse("condo_people:password_reset_complete")
+        self.assertEqual(url, "/condo_people/password-reset/complete/")
