32836cedc9491b535df50864014bfe2061b9a2b0
Rodrigo Farah <digofarah@gmail.com>
2024-08-22 10:03:40 -0300

    adds error messages in case user does not fill in username and/or password fields

--- condo_people/forms/login_form.py
+++ condo_people/forms/login_form.py
@@ -1,6 +1,15 @@
 from django import forms
+from django.contrib.auth.forms import UsernameField
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(required=True)
-    password = forms.CharField(widget=forms.PasswordInput(), required=True)
+    username = UsernameField(
+        widget=forms.TextInput(attrs={"autofocus": True}),
+        error_messages={"required": "Please, inform your username."},
+    )
+    password = forms.CharField(
+        label=("Password"),
+        strip=False,
+        widget=forms.PasswordInput(),
+        error_messages={"required": "You must inform your password"},
+    )
