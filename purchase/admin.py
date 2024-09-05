13f55a86846a6e92b758f9efcb1bca6fd7fb93fb
Rodrigo Farah <digofarah@gmail.com>
2024-09-05 16:47:17 -0300

    Inserts tokens (purchases) in django admin interface

--- purchase/admin.py
+++ purchase/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import RegistrationToken
+
+
+@admin.register(RegistrationToken)
+class RegistrationTokenAdmin(admin.ModelAdmin):
+    list_display = [
+        "register_email",
+        "register_group",
+        "token",
+        "created_at",
+        "expires_at",
+        "not_used_yet",
+    ]
+
+    list_filter = ["register_group"]
