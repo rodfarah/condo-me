8f296828d207c3c72408e2353295f3f5fbb70ff1
Rodrigo Farah <digofarah@gmail.com>
2024-09-18 14:16:42 -0300

    Defines setup_area paths

--- condo/urls.py
+++ condo/urls.py
@@ -6,9 +6,15 @@ app_name = "condo"
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("condominium/", views.condominium, name="condominium"),
-    path("condominium/setup/", views.condominium_setup, name="condominium_setup"),
+    # path("setup-area/condominium", views.setup_condominium, name="setup_condominium"),
+    # path("setup-area/blocks", views.setup_blocks, name="setup_blocks"),
+    # path("setup-area/apartments", views.setup_apartments, name="setup_apartments"),
+    # path(
+    #     "setup-area/common-areas", views.setup_common_areas, name="setup_common_areas"
+    # ),
+    path("setup-area", views.setup_area, name="setup_area"),
     path("condominium/common_areas/", views.common_areas, name="common_areas"),
+    path("condominium/", views.condominium, name="condominium"),
     path(
         "user/profile/settings/",
         views.user_profile_settings,
