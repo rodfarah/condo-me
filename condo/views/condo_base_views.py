4608344449356284dccdc3bcf9b215880915c1f4
Rodrigo Farah <digofarah@gmail.com>
2024-09-24 10:59:02 -0300

    If condominium has a cover picture, it will be sent to welcome template

--- condo/views/condo_base_views.py
+++ condo/views/condo_base_views.py
@@ -4,7 +4,15 @@ from django.shortcuts import render
 
 @login_required(redirect_field_name="redirect_to", login_url="/condo_people/login")
 def home(request):
-    return render(request, template_name="condo/pages/home.html")
+    # send condominum cover path to be rendered in welcome page
+    if request.user.condominium:
+        return render(
+            request,
+            template_name="condo/pages/home.html",
+            context={"condo_cover": request.user.condominium.cover.name},
+        )
+    # if not condominium, no need to send condo cover. Template will use a standard one
+    return render(request, "condo/pages/home.html")
 
 
 @login_required(redirect_field_name="redirect_to", login_url="/condo_people/login")
