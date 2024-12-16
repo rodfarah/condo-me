ba1df12afa0aa49f657763e0b3406b712f64ba06
Rodrigo Farah <digofarah@gmail.com>
2024-12-16 18:12:13 -0300

    Sets up some more tests

--- src/apps/condo/tests/views_tests/test_condo_setup_views.py
+++ src/apps/condo/tests/views_tests/test_condo_setup_views.py
@@ -5,7 +5,11 @@ These tests are DIFFERENT from 'test_condo_views' which are applied to the first
 """
 
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from django.test import TestCase
+from django.urls import reverse
+
+from apps.condo.models import Condominium
 
 
 class CondoSetupViewsTest(TestCase):
@@ -13,15 +17,56 @@ class CondoSetupViewsTest(TestCase):
 
     # setup for starting each test
     def setUp(self) -> None:
-        get_user_model().objects.create_user(
+        super().setUp()
+        # create user
+        self.current_user = get_user_model().objects.create_user(
             first_name="John",
             last_name="Doe",
             email="johndoe@dummy.com",
             username="johndoe",
             password="P@ssw0rd",
         )
-        self.client.login(username="johndoe", password="P@ssw0rd")
-        return super().setUp()
 
-    def test_form_receives_condo_info_via_get_method_views(self):
-        pass
+        # create condominium
+        self.current_condominium = Condominium.objects.create(
+            name="MyCondo",
+            description="Good Condo",
+            cnpj="15306944000169",
+            address1="My Street, 10",
+            address2="Wonderland",
+            city="Soma City",
+            state="Wellness State",
+            country="BR",
+            postal_code="88456123",
+        )
+        # assign condominium to user
+        self.current_user.condominium = self.current_condominium
+
+        # make user belong to "manager" group
+        manager_group, already_exists = Group.objects.get_or_create(name="manager")
+        self.current_user.groups.add(manager_group)
+        self.current_user.save()
+
+        # login user
+        self.client.login(username="johndoe", password="P@ssw0rd")
+
+    def test_manager_group_required_permission_denied(self):
+        # get user in db
+        current_user = get_user_model().objects.first()
+        # create a "test user group" and assign it to current user
+        test_user_group = Group.objects.create(name="test_user_group")
+        current_user.groups.add(test_user_group)
+        # remove "manager" group from setup
+        manager_group = Group.objects.get(name="manager")
+        current_user.groups.remove(manager_group.pk)
+        current_user.save()
+        # user tries to access setup pages
+        response = self.client.get(reverse("apps.condo:condo_setup_home"))
+        self.assertEqual(response.status_code, 403)
+
+    def test_manager_user_with_condo_gets_setup_basic_page_rendered_successfully(self):
+        response = self.client.get(reverse("apps.condo:condo_setup_home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response, "condo/pages/setup_pages/condo_setup_home.html"
+        )
