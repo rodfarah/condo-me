75f0c68626965c36f8a119fb6fcc4879e5747976
Rodrigo Farah <digofarah@gmail.com>
2025-01-27 17:53:19 -0300

    implements tests for setup condominium views

--- src/apps/condo/tests/views_tests/test_condo_setup_views.py
+++ src/apps/condo/tests/views_tests/test_condo_setup_views.py
@@ -6,10 +6,13 @@ These tests are DIFFERENT from 'test_condo_views' which are applied to the first
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
+from django.contrib.messages import get_messages
 from django.test import TestCase
 from django.urls import reverse
 
+from apps.condo.forms import CondoSetupForm
 from apps.condo.models import Condominium
+from apps.condo.views import SetupCondominiumView
 
 
 class BaseTestCase(TestCase):
@@ -113,3 +116,76 @@ class SetupCondominiumViewsTest(BaseTestCase):
         response = self.client.get(path=url)
 
         self.assertEqual(response.context["condo_exists"], False)
+
+    def test_get_form_method_makes_fields_readonly_when_condoinium_exists(self):
+        # get the condominium setup form page
+        url = reverse("condo:condo_setup_condominium")
+        response = self.client.get(path=url)
+
+        # get form from response context
+        form = response.context["form"]
+
+        # check that form fields do not have readonly attribute
+        for field in form.fields.values():
+            self.assertTrue(field.widget.attrs.get("readonly"))
+
+    def test_get_form_method_doesnt_make_fields_readonly_when_condominium_doesnt_exist(
+        self,
+    ):
+        # Delete all existing condominium objects
+        Condominium.objects.all().delete()
+
+        # Get form page considering no condominium oject exists
+        url = reverse("condo:condo_setup_condominium")
+        response = self.client.get(path=url)
+        form = response.context["form"]
+
+        # verify if fields are not readonly
+        for field in form.fields.values():
+            self.assertFalse(field.widget.attrs.get("readonly"))
+
+    def test_form_valid_method_sends_edit_message_if_condominium_already_exists(self):
+        form_data = {
+            "name": "MyBeautifulCondo",  # only different field
+            "description": "Good Condo",
+            "cnpj": "15306944000169",
+            "address1": "My Street, 10",
+            "address2": "Wonderland",
+            "city": "Soma City",
+            "state": "Wellness State",
+            "country": "BR",
+            "postal_code": "88456123",
+        }
+
+        url = reverse("condo:condo_setup_condominium")
+        response = self.client.post(path=url, data=form_data, follow=True)
+
+        # Get messages
+        messages = list(get_messages(response.wsgi_request))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(messages)
+        self.assertEqual(str(messages[0]), "Condominium has been edited successfully")
+
+    def test_form_valid_method_sends_create_message_if_condominium_doesnt_exist(self):
+        Condominium.objects.all().delete()
+
+        form_data = {
+            "name": "MyBeautifulCondo",
+            "description": "Good Condo",
+            "cnpj": "15306944000169",
+            "address1": "My Street, 10",
+            "address2": "Wonderland",
+            "city": "Soma City",
+            "state": "Wellness State",
+            "country": "BR",
+            "postal_code": "88456123",
+        }
+
+        url = reverse("condo:condo_setup_condominium")
+        response = self.client.post(path=url, data=form_data, follow=True)
+
+        messages = list(get_messages(response.wsgi_request))
+
+        self.assertTrue(messages)
+        self.assertEqual(str(messages[0]), "Condominium has been created successfully")
