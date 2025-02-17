d5142612d35e10f2021c3ba589720e5576da6243
Rodrigo Farah <digofarah@gmail.com>
2025-02-17 14:14:24 -0300

    Needs improvements. Blocks must be filtered by condominium

--- src/apps/condo/admin.py
+++ src/apps/condo/admin.py
@@ -1,4 +1,9 @@
+from typing import Any
+
 from django.contrib import admin
+from django.db.models.fields.related import ForeignKey
+from django.forms.models import ModelChoiceField
+from django.http.request import HttpRequest
 
 from apps.condo.models import Apartment, Block, CommonArea, Condominium
 
@@ -25,13 +30,13 @@ class CommonAreaAdmin(admin.ModelAdmin):
 @admin.register(Apartment)
 class ApartmentAdmin(admin.ModelAdmin):
     # items to be shown in users admin main list
-    list_display = ["number_or_name", "block"]
+    list_display = ["number_or_name", "block", "condominium"]
     search_fields = ["number_or_name"]
     # Items to be shown as you choose one apartment
     fields = [
+        "condominium",
         "number_or_name",
         "block",
-        "condominium",
         "display_residents",
     ]
     readonly_fields = ["display_residents"]
@@ -40,3 +45,20 @@ class ApartmentAdmin(admin.ModelAdmin):
         return obj.get_residents()
 
     display_residents.short_description = "Residents"
+
+    # TODO: This is WRONG and needs to be fixed!
+    def formfield_for_foreignkey(
+        self, db_field: ForeignKey, request: HttpRequest, **kwargs: Any
+    ) -> ModelChoiceField | None:
+        if db_field.name == "block":
+            if request.POST.get("condominium"):
+                kwargs["queryset"] = Block.objects.filter(
+                    condominium_id=request.POST.get("condominium")
+                )
+            elif request.GET.get("condominium"):
+                kwargs["queryset"] = Block.objects.filter(
+                    condominium_id=request.GET.get("condominium")
+                )
+            else:
+                kwargs["queryset"] = Block.objects.none()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
